bc33dae5f81100928166e88f4849b8bb55a3a596
mil086 <42316990+mil086@users.noreply.github.com>
2019-12-31 17:16:27 +0100

    Add files via upload

---
+++ esx_rpchat/locales/en.lua
@@ -0,0 +1,14 @@
+Locales['en'] = {
+  ['ooc_prefix'] = 'OOC | @%s^0',
+  ['twt_help'] = 'send a tweet',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^2@^5%s^0)',
+  ['me_help'] = 'personal action',
+  ['me_prefix'] = 'me | %s',
+  ['do_help'] = 'RP information',
+  ['do_prefix'] = 'do | %s',
+  ['news_help'] = 'Shout out some news',
+  ['news_prefix'] = '^0[^3News^0] (^2@^5%s^0)',
+  ['generic_argument_name'] = 'message',
+  ['generic_argument_help'] = 'the message',
+  ['unknown_command'] = '^3%s^0 is not a valid command!',
+}
